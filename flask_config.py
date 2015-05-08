e368a729824ce8d26b7b387e9d1bfffdb1f3d118
soby <sobyx@hotmail.com>
2015-05-08 16:38:14 -0700

    runtime debug setting

--- flask_config.py
+++ flask_config.py
@@ -1,5 +1,12 @@
 import os
 
+def to_bool(val):
+     if isinstance(val, basestring):
+         if val == '1' or val.lower() == 'true':
+             return True
+         return False
+     return bool(val)
+  
 port = int(os.environ.get('PORT', 5000))
 
 # Debug is unsafe, so the default should be debug==False
@@ -9,4 +16,4 @@ if environment == 'LOCAL':
     print "Running locally with debug=True"
     debug = True
 else:
-    debug = False
+    debug = to_bool(os.environ.get('APP_MODE_DEBUG', False))
